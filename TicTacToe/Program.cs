0cfc84e33bcde6e3886128b6f6405268802a5228
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:54:00 -0400

    Added a method to call both players inputs and placement of X or O instead of repeating a switch statement

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -46,9 +46,10 @@ namespace TicTacToe
                                 case 8: playField[2, 1] = 'X'; break;
                                 case 9: playField[2, 2] = 'X'; break;
                             }
+                            break;
 
                         }
-                        break;
+                        
 
 
                 }
@@ -68,6 +69,29 @@ namespace TicTacToe
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
             Console.WriteLine("     |     |     ");
+        }
+
+        public static void EnterXorO(int player, int input)
+        {
+            char playerSign = ' ';
+
+            if (player == 1)
+                playerSign = 'X';
+            else if (player == 2)
+                playerSign = 'O';
+
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
         }
     }
 }
