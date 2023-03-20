76b470aa6706a4b1e3f56a419ab627d0ccce19af
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:30:01 -0400

    Test ran and everything is lined up on field with numbers 1-9

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,13 +6,21 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            
+            SetField();
+            Console.ReadKey();
         }
 
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 4, 5, 6);
+            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 7, 8, 9);
+            Console.WriteLine("     |     |     ");
         }
     }
 }
