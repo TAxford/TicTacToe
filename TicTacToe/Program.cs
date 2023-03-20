9afb6340386dbe87501ab9d9c25a4b12f0e206f5
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:37:24 -0400

    Created the players and numbered the play field

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -4,8 +4,21 @@ namespace TicTacToe
 {
     internal class Program
     {
+        // the playfield
+        static char[,] playField =
+        {
+            {'1','2','3'},
+            {'4','5','6'},
+            {'7','8','9'},
+        };
+
+        //the player
         static void Main(string[] args)
         {
+            int player = 2; //Player 1 starts
+            int input = 0;
+            bool inputCorrect = true;
+
             SetField();
             Console.ReadKey();
         }
@@ -13,13 +26,12 @@ namespace TicTacToe
         public static void SetField()
         {
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0, 0], playField[0, 1], playField[0, 2]);
             Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 4, 5, 6);
-            Console.WriteLine("_____|_____|_____");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);            Console.WriteLine("_____|_____|_____");
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", 7, 8, 9);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
             Console.WriteLine("     |     |     ");
         }
     }
