e642167f5c01b4762446a30941a190a68cbc8e1d
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:57:41 -0400

    Removed the switch stament as it was no longer needed after the creation of the method.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -26,33 +26,16 @@ namespace TicTacToe
             do
             {
                 if (player == 2)
-                    player = 1;
-                else if (player == 1)
-                    player = 2;
-
-                switch (player)
                 {
-                    case 1: //Player 1's turn
-                        {
-                            switch (input)
-                            {
-                                case 1: playField[0, 0] = 'X'; break;
-                                case 2: playField[0, 1] = 'X'; break;
-                                case 3: playField[0, 2] = 'X'; break;
-                                case 4: playField[1, 0] = 'X'; break;
-                                case 5: playField[1, 1] = 'X'; break;
-                                case 6: playField[1, 2] = 'X'; break;
-                                case 7: playField[2, 0] = 'X'; break;
-                                case 8: playField[2, 1] = 'X'; break;
-                                case 9: playField[2, 2] = 'X'; break;
-                            }
-                            break;
-
-                        }
-                        
-
-
+                    player = 1;
+                    EnterXorO(player, input);
                 }
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO(player, input);
+                }               
+                
 
 
             } while (true);
