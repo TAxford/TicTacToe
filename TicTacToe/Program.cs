3848f7699d00508a72ce98db92c795109ff54875
Timothy Axford <t.axford2@yahoo.com>
2023-03-20 13:26:02 -0400

    Starting to create Field

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,13 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public static void SetField()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", 1, 2, 3);
         }
     }
 }
